5d8894c867ac8dbf5bf7279b904490869f87ab59
linda <charming.linda85@gmail.com>
2024-10-03 15:48:26 +0300

    Выполнена задача 1.

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,7 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,10 +27,19 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     @Override
     public int process(List<Update> updates) {
-        updates.forEach(update -> {
-            logger.info("Processing update: {}", update);
-            // Process your updates here
-        });
+        try {
+            updates.forEach(update -> {
+                logger.info("Processing update: {}", update);
+                // Process your updates here
+                if (update.message().text().contains("/start")) {
+                    long chatID = update.message().chat().id();
+                    SendMessage sendMessage = new SendMessage(chatID, "Добро пожаловать в бота!");
+                    telegramBot.execute(sendMessage);
+                }
+            });
+        } catch (RuntimeException e) {
+            logger.error(e.getMessage());
+        }
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
